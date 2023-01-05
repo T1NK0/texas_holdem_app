1c034a4cc0269456caf515b15685fb355d38e3cd
Mads Tinko Madsen <lyderslev1@gmail.com>
2023-01-05 20:41:31 +0100

    Setup of project.

---
+++ lib/pages/gamehub.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+
+class GamehubPage extends StatelessWidget {
+  const GamehubPage({super.key});
+
+  @override
+  Widget build(BuildContext context) => Scaffold();
+}
