39c6ec288341228e215e51b0e4cfebe2c8a74d8b
Mads Tinko Madsen <mtm@itklyngen.dk>
2023-01-09 14:34:40 +0100

    layout of texas table.

--- lib/pages/SevenCardStubGame.dart
+++ lib/pages/SevenCardStubGame.dart
@@ -4,5 +4,43 @@ class SevenCardStubGamePage extends StatelessWidget {
   const SevenCardStubGamePage({super.key});
 
   @override
-  Widget build(BuildContext context) => const Scaffold();
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text('Seven Card Stud'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(children: [
+            Row(
+              children: [
+                Container(
+                    width: 400,
+                    height: 150,
+                    padding: const EdgeInsets.only(
+                        left: 10, top: 20, right: 10, bottom: 0),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                            color: Colors.black,
+                            width: 1,
+                            style: BorderStyle.solid,
+                            strokeAlign: StrokeAlign.inside)),
+                    child: Column(
+                      children: [
+                        Text('1:'),
+                        Text('2:'),
+                        Text('3:'),
+                      ],
+                    )),
+              ],
+            ),
+            SizedBox(),
+            Row(
+              children: [],
+            ),
+            SizedBox(),
+            Row(
+              children: [],
+            ),
+          ]),
+        ),
+      );
 }
